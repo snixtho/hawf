8c8f91e38e69d77e6e8a4cb959463806e6230d3c
snixtho <snixtho@gmail.com>
2022-06-13 10:47:25 +0200

    tests for request builder

--- src/Hawf.Tests/RequestGeneratorTests.cs
+++ src/Hawf.Tests/RequestGeneratorTests.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Hawf.Client;
+using Hawf.Client.Http;
 using Hawf.Utils;
 using Xunit;
 
@@ -34,6 +38,75 @@ public class RequestGeneratorTests
 
         Assert.Equal("/my/path/My+Value", generated);
     }
-    
-    
+
+    [Fact]
+    public void Query_Generated_Correctly()
+    {
+        var request = new ApiRequest
+        {
+            Query = new QueryParamsCollection
+            {
+                { "myParam1", "myValue1" },
+                { "myParam2", 123 }
+            }
+        };
+
+        var query = request.Query.GenerateQuery();
+        
+        Assert.Equal("?myParam1=myValue1&myParam2=123", query);
+    }
+
+    [Fact]
+    public void Query_Values_Are_Encoded()
+    {
+        var request = new ApiRequest
+        {
+            Query = new QueryParamsCollection
+            {
+                { "myParam1", "my value" }
+            }
+        };
+        
+        var query = request.Query.GenerateQuery();
+        
+        Assert.Equal("?myParam1=my+value", query);
+    }
+
+    [Fact]
+    public async Task Content_Body_Text_Content_Generated_Correctly()
+    {
+        var request = new ApiRequest
+        {
+            BodyObject = "My String Value",
+            MimeType = MimeType.Text
+        };
+
+        var body = request.CreateBodyContent();
+        
+        Assert.IsType<StringContent>(body);
+
+        var stringBody = body as StringContent;
+        var value = await stringBody?.ReadAsStringAsync()!;
+        
+        Assert.Equal("My String Value", value);
+    }
+
+    [Fact]
+    public async Task Content_Body_Json_Content_Generated_Correctly()
+    {
+        var request = new ApiRequest
+        {
+            BodyObject = new { MyKey = "MyValue" },
+            MimeType = MimeType.Json
+        };
+
+        var body = request.CreateBodyContent();
+        
+        Assert.IsType<JsonContent>(body);
+
+        var stringBody = body as JsonContent;
+        var value = await stringBody?.ReadAsStringAsync()!;
+        
+        Assert.Equal("{\"myKey\":\"MyValue\"}", value);
+    }
 }
