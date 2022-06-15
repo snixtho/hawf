3336fc50f2309227d11d2872ba1434955ec5a14a
snixtho <snixtho@gmail.com>
2022-06-15 10:00:33 +0200

    add content-type header

--- src/Hawf/Client/Http/HttpHeader.cs
+++ src/Hawf/Client/Http/HttpHeader.cs
@@ -4,4 +4,5 @@ public static class HttpHeader
 {
     public const string UserAgent = "User-Agent";
     public const string Authorization = "Authorization";
+    public const string ContentType = "Content-Type";
 }
